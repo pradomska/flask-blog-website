3152c30f51fd1919bdd6e7a047eaf3ffd7117529
Paulina Radomska <113101087+pradomska@users.noreply.github.com>
2023-05-13 16:07:14 +0200

    Add files via upload

--- blog.py
+++ blog.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 posts = requests.get("https://api.npoint.io/ef6f0bb32d9f2440706b").json()
+OWN_EMAIL = "python32day@outlook.com"
+OWN_PASSWORD = "ezytweyuoinijrht"
 
 app = Flask(__name__)
 
@@ -25,9 +28,25 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        send_email(name, email, phone, message)
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp-mail.outlook.com", port=587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
